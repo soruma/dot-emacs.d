a42eb35ff99d2770e9d532cedb1a31cb624159e3
soruma <soruma.net@gmail.com>
2019-10-25 16:05:10 +0900

    Remove unnecessary packages that are no longer supported

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (twittering-mode swift-mode slim-mode robe rbenv package-utils neotree markdown-mode magit key-chord init-loader ignoramus helm-perspeen helm-dash helm-ag gist gh-md elixir-yasnippets elixir-mode e2wm dired-subtree dired-rainbow dired-open dired-details dash-functional csharp-mode company auto-complete))))
+    (twittering-mode swift-mode slim-mode robe rbenv package-utils neotree markdown-mode magit key-chord init-loader ignoramus helm-perspeen helm-ag gist gh-md elixir-yasnippets elixir-mode e2wm dired-subtree dired-details csharp-mode company auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
