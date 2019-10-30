6b3f121adf75a24b71fdf38c734d4192708dbdba
soruma <soruma.net@gmail.com>
2019-10-30 12:07:21 +0900

    不要なパッケージ管理サイトを削除

--- init.el
+++ init.el
@@ -9,10 +9,8 @@
 
 ;; package管理
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 (require 'cl)
