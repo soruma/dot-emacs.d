531d46105d0375cc3a39d921ea277d5c078aaabd
soruma <soruma.net@gmail.com>
2019-11-21 10:30:49 +0900

    M-g で1語を削除する

--- inits/01_keybind.el
+++ inits/01_keybind.el
@@ -1,5 +1,8 @@
-;; バックスペースの設定
+;; カーソル前の文字を1文字消す
 (global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; カーソル前の1語を消す
+(global-set-key (kbd "M-h") 'backward-kill-word)
 
 ;; ウィンドウ移動
 (global-set-key (kbd "C-c <left>")  'windmove-left)
@@ -8,7 +11,7 @@
 (global-set-key (kbd "C-c <down>")  'windmove-down)
 
 ;; キー移動
-(global-set-key "\M-g" 'goto-line)
+(global-set-key (kbd "M-g") 'goto-line)
 
 ;; ¥の代わりにバックスラッシュを入力する
 (define-key global-map [?¥] [?\\])
