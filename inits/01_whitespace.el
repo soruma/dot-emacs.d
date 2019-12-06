b98c6a0c1114836d6d37acc6094f3798bc96154e
soruma <soruma.net@gmail.com>
2019-12-06 12:56:02 +0900

    タブが表示されていなかったので、表示するように修正

--- inits/01_whitespace.el
+++ inits/01_whitespace.el
@@ -11,8 +11,8 @@
 (setq whitespace-space-regexp "\\(\x3000+\\)")
 (setq whitespace-display-mappings
       '((space-mark ?\x3000 [?\□])
-        (tab-mark   ?\t   [?\xBB ?\t])
+        (tab-mark   ?\t   [?\x00BB ?\t] [?\\ ?t])
         ))
 
-(global-whitespace-mode 1) ; 全角スペースを常に表示
+(global-whitespace-mode 1)
 (global-set-key (kbd "C-x w") 'global-whitespace-mode) ; 全角スペース表示の切替
