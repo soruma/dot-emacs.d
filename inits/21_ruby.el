54a9db96a2c0f2ccfa241f1cfbabdf4c366dea77
soruma <soruma.net@gmail.com>
2020-06-24 18:13:41 +0900

    Setting ruby-mode

--- inits/21_ruby.el
+++ inits/21_ruby.el
@@ -1,6 +1,12 @@
 ;; rbenv setting
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
 (add-hook 'enh-ruby-mode-hook 'yard-mode)
 
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist
              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+
+(setq rbenv-installation-dir (concat (getenv "HOME") "/.rbenv"))
+(setq enh-ruby-program (concat rbenv-installation-dir "/shims/ruby"))
